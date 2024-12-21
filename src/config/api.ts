766863e10e356e2de2032b4f03c7fdc90c698663
Billy Mech <wtmech@proton.me>
2024-12-21 15:57:53 -0600

    change api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,8 @@
+import axios from 'axios';
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+axios.defaults.withCredentials = true;
 
 export const endpoints = {
   inviteSearch: (name: string) => `${API_URL}/api/invites/search?name=${encodeURIComponent(name)}`,
